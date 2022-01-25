3f22008b956617111b77e02690de5f8e41a1c338
Sheri Hansen <sherih84015@gmail.com>
2022-01-25 11:25:06 -0700

    Replace references of IdentityUser to ApplicationUser

--- TaskManagerMVP/Data/ApplicationDbContext.cs
+++ TaskManagerMVP/Data/ApplicationDbContext.cs
@@ -15,35 +15,7 @@ namespace TaskManagerMVP.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<TicketStatus>(x =>
-            {
-                x.HasData(
-                    new TicketStatus() { Id = 1, Name = "New", Description = "New Task", IsActive = true },
-                    new TicketStatus() { Id = 2, Name = "Closed", Description = "Task is Closed", IsActive = true },
-                    new TicketStatus() { Id = 3, Name = "On Hold", Description = "Task is waiting on something to proceed", IsActive = true }
-                    );
-            });
-
-            modelBuilder.Entity<TicketType>(x =>
-            {
-                x.HasData(
-                    new TicketType() { Id = 1, Name = "Bug", Description = "Problem found in code being developed ", IsActive = true },
-                    new TicketType() { Id = 2, Name = "Task", Description = "A Todo to complete", IsActive = true },
-                    new TicketType() { Id = 3, Name = "Issue", Description = "An or cause that may delay release of project ", IsActive = true }
-                    );
-            });
-
-
-
-            modelBuilder.Entity<TicketPriority>(x =>
-            {
-                x.HasData(
-                    new TicketPriority() { Id = 1, Name = "High", Description = "Task is urgent and needs to be done by end of day", IsActive = true },
-                    new TicketPriority() { Id = 2, Name = "Average", Description = "Task should be completed within 3 days", IsActive = true },
-                    new TicketPriority() { Id = 3, Name = "Low", Description = "Task ideally done by end of week", IsActive = true }
-
-                    );
-            });
+            
         }
     }
 }
